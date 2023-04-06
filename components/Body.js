d10320e5fdcf0b2972d30277bfc96a92527f8ead
ImranAhmed26 <17-35401-3@student.aiub.edu>
2023-04-06 23:36:48 +0600

    first commit

---
+++ components/Body.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Body = () => {
+  return <div className="text-5xl font-extrabold px-60 p-10">Body</div>;
+};
+
+export default Body;
---
+++ components/navbar.js
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+import Logo from "../public/assets/logo.png";
+import NavLinks from "../constants/navlinks.js";
+import SearchIcon from "../public/assets/search.svg";
+import FavoriteIcon from "../public/assets/favourite.svg";
+import CartIcon from "../public/assets/cart.svg";
+import ProfileIcon from "../public/assets/profile-image.png";
+
+const Navbar = () => {
+  const [searchText, setSearchText] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [profileName, setProfileName] = useState("");
+
+  useEffect(() => {
+    setProfileImage(ProfileIcon);
+    setProfileName("Anne Doe");
+  });
+
+  const handleSearch = () => {
+    "logic for handling search";
+  };
+
+  return (
+    <div>
+      <div className="w-full h-20 px-12 py-3.5 bg-white text-lg drop-shadow-md flex items-center sticky z-30 top-0">
+        <div className="flex items-center w-1/2">
+          <div>
+            <Image src={Logo} width={57} height={48} alt={"logo"} />
+          </div>
+          <div>
+            <div className=" flex gap-3 px-4 lg:gap-5 xl:gap-7 2xl:gap-9 lg:px-7 xl:px-12 2xl:px-28 text-base lg:text-lg xl:text-xl py-2 ">
+              {NavLinks.navlinks.map((items, i) => {
+                return (
+                  <div key={isNaN}>
+                    <Link href="/">
+                      <a>{items.name}</a>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center w-1/2">
+          <div className="flex items-center justify-end w-full pt-2 pl-2">FORM</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
