0929a05e37e6f0585d621853646f5ab0b2d653aa
ImranAhmed26 <17-35401-3@student.aiub.edu>
2023-04-14 17:31:59 +0600

    added bg image

--- components/navbar.js
+++ components/navbar.js
@@ -15,40 +15,42 @@ const Navbar = () => {
   };
 
   return (
-      <div className="flex justify-center w-full">
-        <div className="flex items-center justify-between w-3/4 bg-amber-300 h-24">
-          <div>
-            <Image src={Logo} width={184} height={48} alt={"logo"} />
-          </div>
-          <div>
-            <div className="text-lg flex gap-9 font-semibold">
-              <Link className="" href={"/"}>
-                <a className="text-color_white hover:text-fuchsia-700 transition-all duration-500">
-                 Mission  
-                </a>
-              </Link>
-              <Link href={"/"}>
-                <a className="text-color_white hover:text-fuchsia-700 transition-all duration-500">
-                 Vision  
-                </a>
-              </Link>
-              <Link href={"/"}>
-                <a className="text-color_white hover:text-fuchsia-700 transition-all duration-500">
-                  Values
-                </a>
-              </Link>
-              <Link href={"/"}>
-                <a className="text-color_white hover:text-fuchsia-700 transition-all duration-500">
-                  Notices
-                </a>
-              </Link>
-            </div>
-          </div>
-          <div className="">
-            <button className="w-24 h-11 rounded-xl bg-fuchsia-700 text-color_white hover:bg-color_white hover:text-fuchsia-700 transition-all duration-500">Forms</button>
+    <div className="flex justify-center w-full bg-main-global h-screen relative z-20 ">
+      <div className="flex items-center justify-between w-3/4  h-24">
+        <div>
+          <Image src={Logo} width={184} height={48} alt={"logo"} />
+        </div>
+        <div>
+          <div className="text-lg flex gap-9 font-semibold">
+            <Link className="" href={"/"}>
+              <a className="text-color_white hover:text-fuchsia-700 transition-all duration-500">
+                Mission
+              </a>
+            </Link>
+            <Link href={"/"}>
+              <a className="text-color_white hover:text-fuchsia-700 transition-all duration-500">
+                Vision
+              </a>
+            </Link>
+            <Link href={"/"}>
+              <a className="text-color_white hover:text-fuchsia-700 transition-all duration-500">
+                Values
+              </a>
+            </Link>
+            <Link href={"/"}>
+              <a className="text-color_white hover:text-fuchsia-700 transition-all duration-500">
+                Notices
+              </a>
+            </Link>
           </div>
         </div>
+        <div className="">
+          <button className="w-24 h-11 rounded-xl bg-fuchsia-700 text-color_white hover:bg-color_white hover:text-fuchsia-700 transition-all duration-500">
+            Forms
+          </button>
+        </div>
       </div>
+    </div>
   );
 };
 
